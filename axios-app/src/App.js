fa9fb51165426e380cef024cceffb387d93b2089
thaisrfernandes <thais.fernandes@ilegra.com>
2020-02-21 12:29:26 -0300

    done

---
+++ axios-app/src/App.js
@@ -0,0 +1,12 @@
+import React from "react";
+import logo from "./logo.svg";
+import "./App.css";
+import DataFetching from "./DataFetching";
+
+const App = () => (
+  <div className="App">
+    <DataFetching />
+  </div>
+);
+
+export default App;
